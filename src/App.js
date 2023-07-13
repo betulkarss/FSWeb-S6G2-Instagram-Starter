3dacac1d25aba9fdb16ecb4532b3918a0812292b
betül kars <betulkars97@gmail.com>
2023-07-13 17:52:50 +0300

    S6G2 ödev teslimi

--- src/App.js
+++ src/App.js
@@ -5,19 +5,38 @@
 */
 
 // State hook u import edin
-import React from "react";
+import React, { useState } from "react";
 
 // Gönderiler (çoğul!) ve AramaÇubuğu bileşenlerini import edin, çünkü bunlar App bileşeni içinde kullanılacak
 // sahteVeri'yi import edin
+import AramaCubugu from "./bilesenler/AramaCubugu/AramaCubugu"
+import Gonderiler from "./bilesenler/Gonderiler/Gonderiler"
+import sahteVeri from "./sahte-veri";
+
 import "./App.css";
 
 const App = () => {
+  const [gonderiler, setGonderiler] = useState(sahteVeri);
+  const [arama, setArama] = useState("");
+  const [begendiklerim, setBegendiklerim] = useState([]);
   // Gönderi nesneleri dizisini tutmak için "gonderiler" adlı bir state oluşturun, **sahteVeri'yi yükleyin**.
   // Artık sahteVeri'ye ihtiyacınız olmayacak.
   // Arama çubuğunun çalışması için , arama kriterini tutacak başka bir state'e ihtiyacımız olacak.
+  const gonderiyiBegen = gonderiID => {
+    const guncellenmisGonderiler = gonderiler.map(post => {
+      if (gonderiID === post.id && !begendiklerim.includes(gonderiID)) {
+        post.likes++;
+        begendiklerim.push(gonderiID);
+        setBegendiklerim(begendiklerim);
+      }
+      return post;
+    });
+    setGonderiler(guncellenmisGonderiler);
+  };
 
-  const gonderiyiBegen = (gonderiID) => {
+  
     /*
+    
       Bu fonksiyon, belirli bir id ile gönderinin beğeni sayısını bir artırma amacına hizmet eder.
 
       Uygulamanın durumu, React ağacının en üstünde bulunur, ancak iç içe geçmiş bileşenlerin stateleri değiştirememesi adil olmaz!
@@ -28,11 +47,22 @@ const App = () => {
         - gönderinin idsi "gonderiID" ile eşleşirse, istenen değerlerle yeni bir gönderi nesnesi döndürün.
         - aksi takdirde, sadece gönderi nesnesini değiştirmeden döndürün.
      */
-  };
-
+        const changeHandler = event => {
+          setArama(event.target.value);
+          const filterGonderiler = sahteVeri.filter(item => {
+            return item.username.includes(event.target.value);
+          });
+          setGonderiler(filterGonderiler);
+        };
+   
   return (
     <div className="App">
-      App Çalışıyor
+      <AramaCubugu
+        arama={arama}
+        setArama={setArama}
+        changeHandler={changeHandler}
+      />
+      <Gonderiler gonderiler={gonderiler} gonderiyiBegen={gonderiyiBegen} />
       {/* Yukarıdaki metni projeye başladığınızda silin*/}
       {/* AramaÇubuğu ve Gönderiler'i render etmesi için buraya ekleyin */}
       {/* Her bileşenin hangi proplara ihtiyaç duyduğunu kontrol edin, eğer ihtiyaç varsa ekleyin! */}
